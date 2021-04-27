963a5bbdd020528517a660bb3308f1a139eb3f62
Alekssoo <losttiger10@gmail.com>
2021-04-27 22:06:05 +0500

    Model Message addad to branch Message

---
+++ models/Message.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app\models;
+
+/**
+ * Description of Message
+ *
+ * @author Alx
+ */
+class Message {
+    public $id;
+    public $text;
+}
